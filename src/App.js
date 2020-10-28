dc44c9eb8be6ba405b620d411059e8b3dfc24c4f
Mike Harvey <harvey.miche@gmail.com>
2020-10-28 22:07:36 +1300

    Adds initial form props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Form from "./components/Form";
+import schema from "../schema.json";
+
 function App() {
   return (
     <div>
       <h1>React form</h1>
+      <Form
+        initialValues={{}}
+        fieldSchema={schema}
+        validationSchema={{}}
+        onSubmit={false}
+      />
     </div>
   );
 }
