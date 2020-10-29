51d0ebf5a535702668b57c4b9e29df32507d3f8e
Mike Harvey <harvey.miche@gmail.com>
2020-10-29 23:12:12 +1300

    Adds Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,31 @@
-function Form() {
-  return <form></form>;
+import InputField from "./InputField";
+
+function renderFields({ schema }) {
+  const fields = [];
+
+  for (const key in schema) {
+    const { type, label } = schema[key];
+
+    fields.push(
+      <InputField type={type} label={label} id={key} key={`field-${key}`} />
+    );
+  }
+
+  return fields;
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log("submit");
+}
+
+function Form({ fieldSchema }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      {renderFields(fieldSchema)}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Form;
